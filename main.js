6eeb0f718af0262805fe66b8b07f2445e93ec39c
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-12-01 16:29:34 -0600

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Hello")
+
+const API_URL ='https://api.thecatapi.com/v1/images/search'
+
+/*fetch(URL)
+  .then(res => res.json())
+  .then(data => {
+    const img = document.querySelector('img')
+    img.src = data[0].url
+  })*/
+async function reload() {
+  const response = await fetch(API_URL)
+  const data = await response.json()
+  const img = document.querySelector('img')
+  img.src = data[0].url
+}
