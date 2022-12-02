d67c972479971a2141ba931df50bb8976ae697bd
Carlos Mata <carlos.lopez@macropay.mx>
2022-12-02 16:49:49 -0600

    update

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-console.log("Hello")
-
-const API_URL ='https://api.thecatapi.com/v1/images/search?limit=3'
-
-console.log(data)
+const API_URL_RANDOM ='https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_ndKMM8eBgLAHgOC21hM5vrn17CdR6AJpMFioRH6qvOIthv9lM2yPzUmDncN5ZAwS';
+const API_URL_FAVORITES ='https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_ndKMM8eBgLAHgOC21hM5vrn17CdR6AJpMFioRH6qvOIthv9lM2yPzUmDncN5ZAwS';
 
 /*fetch(URL)
   .then(res => res.json())
@@ -10,11 +7,38 @@ console.log(data)
     const img = document.querySelector('img')
     img.src = data[0].url
   })*/
-async function reload() {
-  const response = await fetch(API_URL)
-  const data = await response.json()
-  const img = document.querySelector('img')
-  img.src = data[0].url
+
+  const spanError = document.getElementById('error')
+
+async function loadRandomMichis() {
+  const response = await fetch(API_URL_RANDOM);
+  const data = await response.json();
+
+  console.log("Random");
+  console.log(data);
+
+  if (response.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + response.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+  }
 }
 
-reload();
+async function loadFavoritesMichis() {
+  const response = await fetch(API_URL_FAVORITES);
+  const data = await response.json();
+
+  console.log("Favoritos");
+  console.log(data);
+
+  if (response.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + response.status + data.message;
+  }
+}
+
+loadFavoritesMichis();
+loadRandomMichis();
